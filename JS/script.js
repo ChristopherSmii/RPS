f341c70f3fb13177b9b79dca4a597b21279b7b86
Βασίλης Χρήστος Σμυρλόγλου <94610215+ChristopherSmii@users.noreply.github.com>
2024-01-15 19:16:18 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,27 @@ let aiScore=0;
 function cal(playerChoice){
     let choice=Math.floor(Math.random()*3);
     $("h3")[0].innerHTML=playerChoice;
+    if(playerChoice=="Rock"){
+        document.querySelectorAll("h3")[0].classList.remove("greenn");
+        document.querySelectorAll("h3")[0].classList.add("redd");
+        document.querySelectorAll("h3")[0].classList.remove("bluee");        
+    }
+    else if(playerChoice=="Paper"){
+        document.querySelectorAll("h3")[0].classList.remove("greenn");
+        document.querySelectorAll("h3")[0].classList.remove("redd");
+        document.querySelectorAll("h3")[0].classList.add("bluee");  
+    }
+    else{
+        document.querySelectorAll("h3")[0].classList.add("greenn");
+        document.querySelectorAll("h3")[0].classList.remove("redd");
+        document.querySelectorAll("h3")[0].classList.remove("bluee");  
+    }
     switch (choice) {
         case 0:
             //rock
+            document.querySelectorAll("h3")[1].classList.remove("greenn");
+            document.querySelectorAll("h3")[1].classList.add("redd");
+            document.querySelectorAll("h3")[1].classList.remove("bluee");
             $("h3")[1].innerHTML="Rock";
             if(playerChoice=="Rock"){
                 
@@ -22,6 +40,9 @@ function cal(playerChoice){
         case 1:
             //paper
             $("h3")[1].innerHTML="Paper";
+            document.querySelectorAll("h3")[1].classList.remove("greenn");
+            document.querySelectorAll("h3")[1].classList.remove("redd");
+            document.querySelectorAll("h3")[1].classList.add("bluee");
             if(playerChoice=="Rock"){
                 aiScore++;
                 $("h1")[1].innerHTML=aiScore;
@@ -36,6 +57,9 @@ function cal(playerChoice){
             break;
         case 2:
             //scissors
+            document.querySelectorAll("h3")[1].classList.add("greenn");
+            document.querySelectorAll("h3")[1].classList.remove("redd");
+            document.querySelectorAll("h3")[1].classList.remove("bluee");
             $("h3")[1].innerHTML="Scissors";
             if(playerChoice=="Rock"){
                 pScore++;
@@ -55,3 +79,6 @@ $(".option").on("click",function(){
     
 cal(this.innerHTML);
 });
+document.querySelectorAll(".option")[1].classList.add("bluee");
+document.querySelectorAll(".option")[0].classList.add("redd");
+document.querySelectorAll(".option")[2].classList.add("greenn");
